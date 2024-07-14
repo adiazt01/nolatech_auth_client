8bf5c3dbc7602903b309ddaa1591887056a9f0e8
adiazt01 <adiaztovar2004@gmail.com>
2024-07-14 19:07:03 -0400

    chore: Update video section layout and button size

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -26,9 +26,9 @@ export function LandingPage() {
       <CountSection />
       <ProductSection />
       <ServiceSection />
-      <section className="relative px-4 pt-8 flex flex-col lg:flex-row min-h-screen gap-8 justify-center items-end bg-green-600">
-        <div className="flex gap-16 mb-auto lg:mt-20 max-w-lg flex-col px-4">
-          <h2 className="text-3xl text-white md:text-5xl lg:text-7xl font-bold">
+      <section className="relative md:px-4 pt-8 flex flex-col md:flex-row min-h-screen gap-8 justify-center items-center md:items-end bg-green-600">
+        <div className="flex gap-8 lg:gap-16 mb-auto md:mt-20 max-w-lg flex-col px-4">
+          <h2 className="text-4xl text-white text-center md:text-left md:text-5xl lg:text-6xl font-bold">
             Lorem ipsum dolor sit <br />
             amet, consectetur.
           </h2>
@@ -41,7 +41,7 @@ export function LandingPage() {
             </Button>
           </div>
         </div>
-        <div className="flex flex-col  lg:items-end">
+        <div className="flex flex-col">
           <img src="old_man.png" alt="" />
         </div>
       </section>
