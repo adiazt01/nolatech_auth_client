f6ea4bb97664de6f22e3a0fff3bce8abc3be6eb0
adiazt01 <adiaztovar2004@gmail.com>
2024-07-14 20:03:50 -0400

    chore: Update banner top styling and navbar behavior

--- src/components/BannerTop.tsx
+++ src/components/BannerTop.tsx
@@ -2,7 +2,7 @@ import { CirclePlay, Radio } from "lucide-react";
 
 export function BannerTop() {
   return (
-    <div className="flex w-full justify-center gap-4 h-[3.2rem] bg-primary flex-row lg:text-2xl">
+    <div className="flex w-full justify-center gap-4 h-[3rem] bg-primary flex-row lg:text-2xl text-white">
       <div className="h-full flex justify-center flex-row gap-2 px-2 font-semibold items-center bg-red-500 -tracking-tighter">
         <p>LIVE</p>
         <Radio />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,45 @@
 import { Link } from "react-router-dom";
 import { BannerTop } from "./BannerTop";
+import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
 
 /* TODO add navigate */
 export function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY;
+      setIsScrolled(position > window.innerHeight - 120);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
-      className="fixed top-0 text-sm text-center w-full flex flex-col
-    items-center justify-start text-white z-50"
+      className={cn(
+        "fixed top-0 text-sm text-center w-full transition-all border-b border-transparent flex flex-col items-center justify-start z-50",
+        isScrolled
+          ? "bg-white text-green-600 shadow border-gray-100"
+          : "bg-transparent text-white/80"
+      )}
     >
-      {/* TODO El escenario de fondo tiene que cambiar */}
       <BannerTop />
-      <nav className="flex w-full bg-transparent py-1.5 items-center flex-row justify-center">
-        <div className="flex w-full justify-between max-w-6xl text-black">
-          <div className="border border-red-500">
+      <nav className="flex w-full bg-transparent py-2 items-center flex-row justify-center px-2">
+        <div className="flex w-full justify-between max-w-6xl">
+          <div className="">
             <img
               src="/logo.png"
               alt="logo"
-              className="w-22 h-8 md:w-28 md:h-10 lg:w-36 lg:h-14"
+              className="w-22 h-6 md:w-28 md:h-10 lg:w-36 lg:h-14"
             />
           </div>
-          <ul className="flex border border-red-500 flex-row gap-4 items-center justify-center font-medium text-lg">
+          <ul className="flex mt-1 flex-row gap-4 items-center justify-center font-medium text-md">
             <li className="hover:text-gray-100">
               <Link to="/">HOME</Link>
             </li>
