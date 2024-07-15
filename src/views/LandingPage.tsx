774ad9e63ec143c4d877b02ed59f0b99221c530c
adiazt01 <adiaztovar2004@gmail.com>
2024-07-14 20:23:49 -0400

    chore: Update image sizes in Navbar and LandingPage

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -25,8 +25,8 @@ export function LandingPage() {
       <CountSection />
       <ProductSection />
       <ServiceSection />
-      <section className="relative md:px-4 pt-8 flex flex-col md:flex-row gap-8 justify-center items-center md:items-end bg-green-700">
-        <div className="flex gap-8 lg:gap-16 mb-auto lg:mt-20 max-w-lg flex-col px-4">
+      <section className="relative md:px-4 pt-24 flex flex-col md:flex-row gap-8 justify-center items-center md:items-end bg-green-700">
+        <div className="flex gap-8 lg:gap-8 mb-auto max-w-lg flex-col px-4">
           <h2 className="text-4xl text-white text-center md:text-left md:text-5xl lg:text-6xl font-bold">
             Lorem ipsum dolor sit <br />
             amet, consectetur.
@@ -41,7 +41,7 @@ export function LandingPage() {
           </div>
         </div>
         <div className="flex flex-col">
-          <img src="old_man.png" alt=""  className="max-w-xs mx-auto"/>
+          <img src="old_man.png" alt=""  className="max-w-xs mx-auto md:max-w-sm lg:max-w-lg"/>
         </div>
       </section>
       <VideoSection />
